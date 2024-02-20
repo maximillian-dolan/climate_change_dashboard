6a6bf4ec6768db54509ad70cf9f0ef6d20f007a5
maxdolan <maxdolan@MacBook-Pro-106.local>
2024-02-20 19:01:37 +0000

    added multiple years for fire_data and made it a bit neater

--- precipi_streamlit.py
+++ precipi_streamlit.py
@@ -5,13 +5,33 @@ import plotly.express as px
 import os
 from datetime import datetime
 
-#importing fire data
+
+
+# Importing fire data
 def fire_stringcut(a):
     return a[5:7]
 
-fire_df = pd.read_csv('./modis_2015_United_States.csv')
-fire_df['month'] = fire_df['acq_date'].apply(fire_stringcut)
-fire_df['confidence'] = fire_df['confidence']/100
+# CSV file names
+fire_folder_path = './USA_fire_date_2010_2023'
+csv_fire_files = [file for file in os.listdir(fire_folder_path) if file.endswith('.csv')]
+
+# Loop through each CSV file and create a dataframe for said file, restricting to rough california coordinates
+fire_dataframes = {}
+for csv_file in csv_fire_files:
+
+    year = int(csv_file.split('_')[2].split('.')[0])
+    
+    whole_fire_df = pd.read_csv(os.path.join(fire_folder_path, csv_file))
+    fire_dataframes[f'{year}'] = whole_fire_df[whole_fire_df['latitude'] < 42][whole_fire_df['latitude']>33][whole_fire_df['longitude']<-115]
+
+# Format Dataframes
+for fire_df in fire_dataframes.values():
+    fire_df['month'] = fire_df['acq_date'].apply(fire_stringcut)
+    fire_df['confidence'] = fire_df['confidence']/100
+    fire_df['year'] = fire_df['year'].apply(str)
+
+fire_all_data = pd.concat(fire_dataframes, ignore_index=True)
+
 
 
 def home_page():
@@ -29,11 +49,8 @@ def precipitation_page():
     # daily or monthly data type
     data_type = st.radio("Select data type", ('daily', 'monthly'))
 
-    # checkbox to show fires
-    if data_type == 'daily':
-        show_fires = st.checkbox(label = 'Show Fire data')
-    else:
-        show_fires = False
+    # checkbox to show fires  
+    show_fires = st.checkbox(label = 'Show Fire data')
 
     # set the folder path of csv file
     folder_path = f'./csv/{data_type}'
@@ -56,7 +73,8 @@ def precipitation_page():
     )
 
     # filter by date slider and rough california boundaries
-    fire_df2 = fire_df[fire_df['latitude'] < 42][fire_df['latitude']>33][fire_df['longitude']<-115][fire_df['acq_date'] == str(selected_date)[:10] ]
+    selected_year = str(selected_date)[:4]
+    fire_dataframe = fire_dataframes[selected_year]
 
     # Select date
     if selected_date:
@@ -87,19 +105,23 @@ def precipitation_page():
                 title=f'Precipitation for {date_str}'
             )
 
-            # create fire plot      
-                
-            if show_fires == True and data_type == 'daily':
-                fig.add_trace(px.scatter_mapbox(fire_df2,
+            # create fire plot                  
+            if data_type == 'daily':
+                fire_dataframe_date = fire_dataframe[fire_dataframe['acq_date'] == str(selected_date)[:10]]
+            elif data_type == 'monthly':
+                fire_dataframe_date = fire_dataframe[fire_dataframe['month'] == str(selected_date)[5:7]]
+
+            if show_fires == True:
+                fire_dataframe_date = fire_dataframe[fire_dataframe['acq_date'] == str(selected_date)[:10]] if data_type == 'daily' else fire_dataframe[fire_dataframe['month'] == str(selected_date)[5:7]]
+                fig.add_trace(px.scatter_mapbox(fire_dataframe_date,
                                                 lat='latitude',
                                                 lon='longitude',
-                                                color_discrete_sequence=['red']*len(fire_df2),
+                                                color_discrete_sequence=['red']*len(fire_dataframe_date),
                                                 mapbox_style='open-street-map',
                                                 zoom=4,
                                                 title=f'Fire locations'  
                                                 ).data[0]
                             )
-            
 
             # Show
             st.plotly_chart(fig)
@@ -113,40 +135,68 @@ def fire_page():
     st.header("Fire Occurence Data")
     st.write("data and visualizations")
 
-    #tab1, tab2 = st.tabs(['locations','frequency'])
+    # prepare fire frequency dataframes
+    firecount_dataframes = {}
 
-    #import and prepare fire data
-    california_fire_data = fire_df[fire_df['latitude'] < 42][fire_df['latitude']>33][fire_df['longitude']<-115]
-    fire_month_counts = pd.DataFrame(data = california_fire_data.value_counts(subset = 'month', sort = False))
-    fire_month_counts['month names'] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    for df in fire_dataframes:
+        specific_month_counts = pd.DataFrame(data = fire_dataframes[df].value_counts(subset = 'month', sort = False))
+        specific_month_counts['month names'] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        specific_month_counts['year'] = df
+        specific_month_counts['count'] = specific_month_counts['count']/len(fire_dataframes[df])
 
-    #Create fire locations plot
-    fig_fire = px.scatter_mapbox(california_fire_data,
-                            lat='latitude',
-                            lon='longitude',
-                            color_discrete_sequence=['red']*len(fire_df),
-                            mapbox_style='open-street-map',
-                            zoom=4,
-                            hover_name = 'confidence',
-                            opacity = california_fire_data['confidence'],
-                            animation_frame = 'month',
-                            title=f'Fire locations')
+        firecount_dataframes[df] = specific_month_counts
 
-    #Create fire frequency chart
-    fire_frequency_chart = px.bar(data_frame = fire_month_counts,
-                                    x = 'month names',
-                                    y = 'count',
-                                    hover_name = 'month names',
-                                    color_discrete_sequence = ['red']*12)
-    
+    all_fire_frequencies = pd.concat(firecount_dataframes, ignore_index=True)
 
     #Create tabs for two plots
     tab1, tab2 = st.tabs(['locations','frequency'])
 
+    #Create fire locations plot
     with tab1:
+
+        fire_data_type = st.radio('select data type',('monthly','yearly'))
+
+        if fire_data_type == 'monthly':
+            fire_year = st.select_slider('year', options = fire_dataframes.keys())
+            fire_df_chosen = fire_dataframes[fire_year]
+
+        if fire_data_type == 'monthly':
+
+            fig_fire = px.scatter_mapbox(fire_df_chosen,
+                                    lat='latitude',
+                                    lon='longitude',
+                                    color_discrete_sequence=['red']*len(fire_df),
+                                    mapbox_style='open-street-map',
+                                    zoom=3,
+                                    hover_name = 'confidence',
+                                    opacity = fire_df_chosen['confidence'],
+                                    animation_frame = 'month',
+                                    title=f'Fire locations')
+
+        if fire_data_type == 'yearly':
+            fig_fire = px.scatter_mapbox(fire_all_data,
+                                    lat='latitude',
+                                    lon='longitude',
+                                    color_discrete_sequence=['red']*len(fire_df),
+                                    mapbox_style='open-street-map',
+                                    zoom=3,
+                                    hover_name = 'confidence',
+                                    opacity = fire_all_data['confidence'],
+                                    animation_frame = 'year',
+                                    title=f'Fire locations') 
+        
         st.plotly_chart(fig_fire)
         
-    
+    # Create fire frequency chart
+    fire_frequency_chart = px.bar(data_frame = all_fire_frequencies,
+                                        x = 'month names',
+                                        y = 'count',
+                                        hover_name = 'month names',
+                                        animation_frame  = 'year',
+                                        animation_group = 'month names',
+                                        range_y = [0,0.7],
+                                        color_discrete_sequence = ['red']*len(all_fire_frequencies))
+
     with tab2:
         st.plotly_chart(fire_frequency_chart)
 
