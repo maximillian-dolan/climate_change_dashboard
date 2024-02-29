e871a7d436c9e3e9ca00e0510a9c1bd89ccf8b53
IshaqBRST <149443761+IshaqBRST@users.noreply.github.com>
2024-02-29 06:24:19 +0000

    Update precipi_streamlit.py
    Humidity now included in streamlit

--- precipi_streamlit.py
+++ precipi_streamlit.py
@@ -38,11 +38,83 @@ def home_page():
 
 def humidity_page():
     st.header("Humidity Data")
-    st.write("Humidity data and visualizations go here...")
+    st.write("Explore Humidity Data")
+    
+    # daily or monthly data type
+    precipitation_data_type = st.radio("Select Data Type", ('Daily', 'Monthly'))
+
+    # checkbox to show fires  
+    show_fires = st.checkbox(label = 'Show Fire Data')
+    
+    # set the folder path of csv file
+    base_directory = "./humidity_data"
+    humidity_folder_path = f'{base_directory}/{humidity_data_type.lower()}'
+
+    #humidity_file_paths = [
+        #os.path.join(humidity_folder_path, f) for f in os.listdir(humidity_folder_path)
+        #if f.endswith('.csv')
+    #]
+
+ # Get the list of dates from the file names
+    humidity_dates = [
+        datetime.strptime(os.path.splitext(f)[0], '%Y-%m-%d') for f in os.listdir(humidity_folder_path) if f.endswith('.csv')
+    ]
+    humidity_dates.sort()
+
+    # Create slider
+    selected_date = st.select_slider(
+        'Select a date',
+        options=humidity_dates,
+        format_func=lambda date: date.strftime('%Y-%m-%d')
+    )
+
+    # filter by date slider
+    if selected_date:
+        # define date_str and humidity_file_path
+        date_str = selected_date.strftime('%Y-%m-%d')
+        humidity_file_path = os.path.join(humidity_folder_path, f"{date_str}.csv")
+
+        if os.path.exists(humidity_file_path):
+            # Read csv file
+            humidity_df = pd.read_csv(humidity_file_path)
+
+            # set the confidence level
+            humidity_confidence_level = 0.95
+            humidity_color_scale_max = humidity_df['Q_air_f_inst'].quantile(humidity_confidence_level)
+
+            # Create map
+            fig_humidity = px.scatter_mapbox(
+                humidity_df,
+                lat='lat',
+                lon='lon',
+                size='Q_air_f_inst',
+                color='Q_air_f_inst',
+                color_continuous_scale=px.colors.sequential.Viridis,
+                range_color=(humidity_df['Q_air_f_inst'].min(), humidity_color_scale_max),
+                mapbox_style='open-street-map',
+                zoom=5,  
+                title=f'Humidity for {date_str}'
+            )
+
+            if show_fires == True:
+                fire_dataframe_date = fire_dataframe[fire_dataframe['acq_date'] == str(selected_date)[:10]] if precipitation_data_type == 'daily' else fire_dataframe[fire_dataframe['month'] == str(selected_date)[5:7]]
+                fig_precipitation.add_trace(px.scatter_mapbox(fire_dataframe_date,
+                                                lat='latitude',
+                                                lon='longitude',
+                                                color_discrete_sequence=['red']*len(fire_dataframe_date),
+                                                mapbox_style='open-street-map',
+                                                zoom=4,
+                                                title=f'Fire locations'  
+                                                ).data[0]
+
+            
+            st.plotly_chart(fig_humidity)
+
+humidity_page()
 
 def precipitation_page():
     st.header("Precipitation Data")
-    st.write("Explore precipitation data.")
+    st.write("Explore Precipitation Data.")
 
     # daily or monthly data type
     precipitation_data_type = st.radio("Select data type", ('daily', 'monthly'))
