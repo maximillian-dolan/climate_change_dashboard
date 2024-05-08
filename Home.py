979cb6519f62151308ec4720b080d4f4f3fde498
ruyuG <147399330+ruyuG@users.noreply.github.com>
2024-05-08 02:22:25 +0100

    Update Home.py
    update clean cache function

--- Home.py
+++ Home.py
@@ -220,7 +220,7 @@ def home_page():
      If you upload a new data file, click that button to clear the cache.
        """)
     if st.button('Clear Cache'):
-        st.caching.clear_cache()
+        streamlit.legacy_caching.clear_cache()
         st.success('Cache cleared! Please rerun the app.')
 
 def main():
